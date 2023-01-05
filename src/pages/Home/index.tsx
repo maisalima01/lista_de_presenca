3005dec07357e9b498af2c2450ac208715d42496
Maísa Lima <maisafernanda@hotmail.com>
2023-01-05 15:40:22 -0300

    alterando projeto em JS para TS

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
 
-import { Card } from '../../components/Card';
+import { Card, CardProps } from '../../components/Card';
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Home() {
   const [studentName, setStudentName] = useState('');
-  const [students, setStudents] = useState([]);
-  const [user, setUser] = useState({ name: '', avatar: '' });
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User);
 
   function handleAddStudent() {
     const newStudent = {
@@ -25,9 +35,8 @@ export function Home() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('https://api.github.com/users/rodrigorgtic');
-      const data = await response.json();
-      console.log("DADOS ===> ", data);
-
+      const data = await response.json() as ProfileResponse;
+    
       setUser({
         name: data.name,
         avatar: data.avatar_url,
@@ -43,7 +52,7 @@ export function Home() {
         <h1>Lista de Presença</h1>
 
         <div>
-          <strong>{user.name}</strong>
+          <strong>{user.avatar}</strong>
           <img src={user.avatar} alt="Foto de perfil" />
         </div>
       </header>
